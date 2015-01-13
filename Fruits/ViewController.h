ddcbf5fe07515496e75dd2d25bd22db4039a7e7a
Hawa Singh Kumawat <hawa.singh.kumawat@accenture.com>
2015-01-13 11:11:40 +0530

    Initial Commit

---
+++ Fruits/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fruits
+//
+//  Created by hawa.singh.kumawat on 13/01/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fruits/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fruits
+//
+//  Created by hawa.singh.kumawat on 13/01/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
